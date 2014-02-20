4e372f915dbade30f9d156ea23a52322b210fe23
dsolis5323 <dsolis5323>
2014-02-20 09:40:08 -0600

    Finish comparison of dates for print
    Print the commits with a comparison of dates, this is a test commit (4h)

--- models/commit.js
+++ models/commit.js
@@ -11,44 +11,46 @@ var path = require('path'),
 var commit = {
 //'../../ttlogn-tool'
 
-	displayCommits: function(ppath, pdateLimit, pgitName){
-		var self = this;
-		dataAccess.getBranches(ppath, pdateLimit, pgitName);
+	displayCommits: function(ppath, pdate, pgitName){
+		if(pdate === '-w' || pdate === '-m' || pdate === '-d' || typeof pdate === 'undefined'){
+			dataAccess.getBranches(ppath, pdate, pgitName);
+		}
+		else{
+			console.log('ttlogn ls [-d/-w/-m]');
+		}
 	},
 
-	printBranches: function(ppath, parray, pdateLimit, pgitName){
+	printBranches: function(ppath, parray, pdate, pgitName){
 		_.each(parray,function(value,index){
-			dataAccess.getLastCommits(ppath, value.name, 2, pdateLimit, pgitName);
+			dataAccess.getLastCommits(ppath, value.name, 10, pdate, pgitName);
 		});
 	},
 
 	/* parray: array of commits
 	prints the information of the commits */
 	printCommitsList: function(parray, pdate, pgitName){
-		var limitDate = new Date();
+	var limitDate,
+		commitDate;
 
-		if(pdate === '-d' || typeof pdate === 'undefined'){
-			limitDate.setHours(limitDate.getHours() - limitDate.getHours());
-			console.log(limitDate.getDate());
-		}
-		else if(pdate === '-w'){
-			console.log(limitDate.getDay());
-			limitDate.setDate(limitDate.getDay() - limitDate.getDay() + 1);
-			console.log(limitDate.getDate());
+	limitDate = new Date();
+	limitDate = new Date(limitDate.getFullYear(),limitDate.getMonth(), limitDate.getDate());
+
+		if(pdate === '-w'){
+			limitDate.setDate(limitDate.getDate() - limitDate.getDay() + 1);
 		}
 		else if(pdate === '-m'){
 			limitDate.setDate(limitDate.getDate() - limitDate.getDate() + 1);
-			console.log(limitDate.getDate());
 		}
+	
 		_.each(parray,function(value,index){
-			if(	value.author.name === pgitName && Date(value.committed_date) >= limitDate){
+
+			if(	value.author.name === pgitName && value.committed_date >= limitDate){
 				console.log(value.author.name);
-				console.log(Date(value.committed_date.getDate()));
+				console.log(value.committed_date);
 				console.log(limitDate);
 				console.log(value.message);
 				console.log('-----------------------');
 			}
-		
 		});
 
 	}
