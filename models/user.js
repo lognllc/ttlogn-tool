9c9ba04c26d8e790e86e45752d3390c785e51655
dsolis5323 <dsolis5323>
2014-04-23 12:44:12 -0600

    Deleted login with google

--- models/user.js
+++ models/user.js
@@ -4,9 +4,6 @@ var //_ = require('underscore'),
 	RSVP = require('rsvp'),
 	_ = require('underscore'),
 	colog = require('colog'),
-	config = require(path.resolve(__dirname,'../lib/oauth2-config.js')),
-	goauth2 = require('google-oauth2')(config),
-	//OAuth = require(path.resolve(__dirname,'../node_modules/google-oauth/GoogleOAuth')),
 	pivotalDataAccess = require(path.resolve(__dirname,'../dataAccess/pivotal_data_access.js')),
 	dataAccess = require(path.resolve(__dirname, '../dataAccess/apitt_data_access.js'));
 
@@ -14,64 +11,12 @@ var USER_LOGIN = '/login/create.json',
 	GET_CLIENTS = '/users/get_clients.json?id=',
 	PERIOD = '/users/get_actual_period.json?id=';
 
-var scope = "https://www.googleapis.com/auth/userinfo.profile";
-
-
 var user = {
-
-	//googleLogin: function (){
-	login: function (){
-		var promise = new RSVP.Promise(function(resolve, reject){
-			goauth2.getAuthCode(scope, function(err, auth_code){
-				console.log(auth_code);
-				console.log(err);
-				if(!err){
-					resolve(auth_code);
-				}
-				else
-				{
-					reject(err);
-				}
-			});
-		});
-		return promise;
-/*		var oauth = new OAuth.OAuth2(
-			'606947730010.apps.googleusercontent.com',
-			'8GDk4I2SRnLD2PugicAgrvE7',
-			'http://localhost:8082/authentication');
-
-		server.get('/authentication', function(req, res){
-
-			if(!req.query.code){
-				//Redirect the user to Authentication From,
-				// Set authentication scope to google calendar api
-				oauth.getGoogleAuthorizeTokenURL( ['https://www.googleapis.com/auth/userinfo.email'], function(err, redirecUrl) {
-					if(err)
-					{
-						return res.send(500,err);
-					}
-					return res.redirect(redirecUrl);
-				});
-
-			}else{
-				//Get access_token from the code
-				oauth.getGoogleAccessToken(req.query, function(err, access_token, refresh_token) {
-					if(err){
-						return res.send(500,err);
-					}
-					req.session.access_token = access_token;
-					req.session.refresh_token = refresh_token;
-					return res.redirect('/');
-				});
-			}
-		});
-	*/
-	},
 
 	/*puser: email of the user
 	ppassword: encrypted password of the user
-	gets the information of the user in the Timetracker*/
-	login2: function(puser, ppassword){
+	gets the information of the user in the gTimetracker*/
+	login: function(puser, ppassword){
 		var message	= {
 				email: puser,
 				password: ppassword
