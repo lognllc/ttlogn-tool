7e7c1cc18d38385d60e9c9151da25b9731dbfc75
Daniel Solis <danielsolis.logn@gmail.com>
2014-11-11 21:27:19 -0600

    Update Git User message to Commit Author

--- models/config.js
+++ models/config.js
@@ -93,7 +93,7 @@ var config = {
 					hidden: true
 				},
 				gitUser:{
-					description: 'Git user'.magenta,
+					description: 'Commit Autor'.magenta,
 					required: true
 				},
 				pivotalEmail:{
